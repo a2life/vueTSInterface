f3ea55dc72ac37db888461fa9268663857233d91
10032268 <masaki.adachi@us.yazaki.com>
2019-03-27 18:19:00 -0400

    2-3/4 build. moment date formatting and bootstrap decoration

--- src/AppointmentListItem.vue
+++ src/AppointmentListItem.vue
@@ -1,23 +1,78 @@
 <template>
-    <li>
-        <div>{{appointment.petName}}</div>
-        <div>{{appointment.petOwner}}</div>
-        <div>{{appointment.aptDate}}</div>
-        <div>{{appointment.aptNotes}}</div>
-    </li>
+    <li class="pet-item media">
+
+        <div class="media-left">
+            <button class="pet-delete btn btn-xs btn-danger">
+                <span class="glyphicon glyphicon-remove"></span></button>
+        </div><!-- media-left -->
+
+        <div class="pet-info media-body">
+
+            <div class="pet-head">
+                <span class="pet-name">{{appointment.petName}}</span>
+                <span class="apt-date pull-right">{{this.formattedDate}}</span>
+            </div><!-- pet-head -->
+
+            <div class="owner-name">
+                <span class="label-item">Owner:</span>
+                <span>{{appointment.petOwner}}</span>
+            </div>
+            <div class="apt-notes">{{appointment.aptNotes}}</div>
+
+        </div><!-- pet-info -->
+
+    </li><!-- pet-item -->
+
 </template>
 
 <script lang="ts">
     import Vue from 'vue';
-   interface Iapp {petName:String; petOwner:String; aptDate:String; aptNotes:String }
+    import * as moment from 'moment';
+   interface app {petName:string; petOwner:string; aptDate:string; aptNotes:string }
     export default Vue.extend( {
         name: "AppointmentListItem",
         props: {appointment:{
-            type: Object as ()=>Iapp
-            }}
+            type: Object as ()=>app
+            }},
+        computed: {
+            formattedDate: function(){
+               return moment(this.appointment.aptDate).format('MM-DD-YY, h:mm a');
+
+            }
+        }
     })
 </script>
 
 <style scoped>
+    .pet-item {
+        border-bottom: 1px dotted gray;
+        padding-bottom: 10px;
+    }
+    .pet-item:last-child {
+        border-bottom: none;
+    }
+    .pet-name {
+        font-weight: 600;
+        color: #337ab7;
+        font-size: 1.3em;
+        line-height: 100%;
+    }
+    .pet-info {
+        font-size: 1.2em;
+    }
+    .label-item {
+        font-weight: 600;
+        color: #667B82;
+    }
 
+    .pet-delete {
+        font-size: 1.2em;
+    }
+    .apt-date {
+        font-style: italic;
+    }
+    [contenteditable]:focus {
+        outline: none;
+        background: #EEE8D6;
+    }
 </style>
