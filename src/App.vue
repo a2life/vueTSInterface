e51cd61e072b4c78a9d578deae56ce141c3d16ad
10032268 <masaki.adachi@us.yazaki.com>
2019-04-02 13:44:09 -0400

    4-1 added search Appointment component. html only, not wired up to real search yet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="main-app">
         <add-appointment @addRecord="addAppointment" ></add-appointment>
+        <search-appointment></search-appointment>
         <appointment-list v-bind:appointments = 'appointments' @removeRecord="removeAppointment"></appointment-list>
 
     </div>
@@ -10,6 +11,7 @@
  import Vue from 'vue';
  import aList from './AppointmentsList.vue';
  import addAppointment from './AddAppointment.vue';
+ import searchAppointment from './SearchAppointment.vue';
  import * as _ from 'lodash';
  interface app {petName:string; petOwner:string; aptDate:string; aptNotes:string }
 
@@ -22,7 +24,8 @@
         },
         components: {
             'appointment-list': aList,
-            'add-appointment':addAppointment
+            'add-appointment':addAppointment,
+            'search-appointment':searchAppointment
         },
         created(): void {
             $.getJSON('appointments.json')
